d2985832e3c39e9eea144074bab68ba3d668afd5
ch0105 <150002102+ch0105@users.noreply.github.com>
2024-06-07 00:37:25 +0800

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="index.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.html"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>other links</h3>
+         <a href="index.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.html"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:9313377651"><i class="fas fa-phone"></i> +6281343552984</a>
+         <a href="tel:7304273313"><i class="fas fa-phone"></i> +6285156660105</a>
+         <a href="mailto:preetmoga777@gmail.com"><i class="fas fa-envelope"></i> kelompoksks11@gmail.com</a>
+         <a href=""><i class="fas fa-map-marker-alt"></i> wakanda, tanzania utara</a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> | <span>Kelompok SKS</span> | all rights reserved!</div>
+
+</footer>
---
+++ script.js
@@ -0,0 +1,20 @@
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+         slidesPerView: 1,
+      },
+      768: {
+         slidesPerView: 2,
+      },
+      991: {
+         slidesPerView: 3,
+      },
+   },
+});
